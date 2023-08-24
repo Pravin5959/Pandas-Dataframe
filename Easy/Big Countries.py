dff1d9f84b51a4692505df7f550670c8dff3a4f5
Pravin Yadav <61359326+Pravin5959@users.noreply.github.com>
2023-08-24 22:36:34 +0530

    Update Big Countries.py

--- Easy/Big Countries.py
+++ Easy/Big Countries.py
@@ -51,6 +51,9 @@ Explanation :
 Step1 : This problem require us to return a filtered dataframe with population >= 25000000 and area >= 3000000
 Step2 : In Pandas, we apply filter while row by row parsing of the index and their values
 
+Schema : 
+data = [['Afghanistan', 'Asia', 652230, 25500100, 20343000000], ['Albania', 'Europe', 28748, 2831741, 12960000000], ['Algeria', 'Africa', 2381741, 37100000, 188681000000], ['Andorra', 'Europe', 468, 78115, 3712000000], ['Angola', 'Africa', 1246700, 20609294, 100990000000]]
+World = pd.DataFrame(data, columns=['name', 'continent', 'area', 'population', 'gdp']).astype({'name':'object', 'continent':'object', 'area':'Int64', 'population':'Int64', 'gdp':'Int64'})
 '''
 
 import pandas as pd
