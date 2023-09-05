2410faec51a7bfe3fd746fc959ff50780e22da84
Pravin Yadav <61359326+Pravin5959@users.noreply.github.com>
2023-09-05 23:00:19 +0530

    Create Find Users With Valid E-Mails.py

---
+++ Easy/Find Users With Valid E-Mails.py
@@ -0,0 +1,81 @@
+'''
+Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| name          | varchar |
+| mail          | varchar |
++---------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains information of the users signed up in a website. Some e-mails are invalid.
+ 
+
+Write a solution to find the users who have valid emails.
+
+A valid e-mail has a prefix name and a domain where:
+
+The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+The domain is '@leetcode.com'.
+Return the result table in any order.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Users table:
++---------+-----------+-------------------------+
+| user_id | name      | mail                    |
++---------+-----------+-------------------------+
+| 1       | Winston   | winston@leetcode.com    |
+| 2       | Jonathan  | jonathanisgreat         |
+| 3       | Annabelle | bella-@leetcode.com     |
+| 4       | Sally     | sally.come@leetcode.com |
+| 5       | Marwan    | quarz#2020@leetcode.com |
+| 6       | David     | david69@gmail.com       |
+| 7       | Shapiro   | .shapo@leetcode.com     |
++---------+-----------+-------------------------+
+Output: 
++---------+-----------+-------------------------+
+| user_id | name      | mail                    |
++---------+-----------+-------------------------+
+| 1       | Winston   | winston@leetcode.com    |
+| 3       | Annabelle | bella-@leetcode.com     |
+| 4       | Sally     | sally.come@leetcode.com |
++---------+-----------+-------------------------+
+Explanation: 
+The mail of user 2 does not have a domain.
+The mail of user 5 has the # sign which is not allowed.
+The mail of user 6 does not have the leetcode domain.
+The mail of user 7 starts with a period.
+
+pandas schema : data = [[1, 'Winston', 'winston@leetcode.com'], [2, 'Jonathan', 'jonathanisgreat'], [3, 'Annabelle', 'bella-@leetcode.com'], [4, 'Sally', 'sally.come@leetcode.com'], [5, 'Marwan', 'quarz#2020@leetcode.com'], [6, 'David', 'david69@gmail.com'], [7, 'Shapiro', '.shapo@leetcode.com']]
+users = pd.DataFrame(data, columns=['user_id', 'name', 'mail']).astype({'user_id':'int64', 'name':'object', 'mail':'object'})
+
+Note : I've done this question without regex, you can solve this question easily if you know regex
+'''
+import pandas as pd
+
+def check_emails_validity(s):
+    check = ['_', '.', '-']
+    s = s.upper()
+    if len(s) > 13:
+        if ''.join(reversed(s[-1:-14:-1])) == '@LEETCODE.COM' and ord(s[0]) in range(65,91):
+            flg = 0
+            for i in list(s[:len(s)-13]):
+                st = ord(i)
+                if st in range(65,91): continue
+                elif st in range(48,59): continue
+                elif i in check: continue
+                else: return 0
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    users['flag'] = users['mail'].apply(lambda x : check_emails_validity(x))
+    df = users[users['flag'] == 1]
+    return df[['user_id', 'name', 'mail']]
